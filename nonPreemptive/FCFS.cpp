c5e7748dd2b53bfc110f06f494581aef9451b85b
vasundhra gupta <vasundhragupta962@gmail.com>
2025-04-10 12:00:46 +0530

    FCFS

--- nonPreemptive/FCFS.cpp
+++ nonPreemptive/FCFS.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include "../headers/process.h"
+#include "../headers/maxHeap.h"
 using namespace std;
+//💡Concept💡
+//
 
-void CT(vector<Process> &processes, int n)
+void FCFS(vector<Process> &processes, int n)
 {
+    heapSort(processes);
     int currentTime = 0;
     for (int i = 0; i < n; i++)
     {
         currentTime = max(currentTime, processes[i].AT) + processes[i].BT;
         processes[i].CT = currentTime;
     }
+    WT_TT(allProcesses);
+    showProcess(allProcesses);
 }
 
 int main()
 {
-    // cout << "Enter the number of processes:" << endl;
     int n = 5;
+    // cout << "Enter the number of processes:" << endl;
     // cin >> n;
     // vector<Process> processes(n, {0, 0, 0});
-    vector<Process> processes = {
-        {1, 0, 10},
-        {2, 1, 1},
-        {3, 2, 2},
-        {4, 3, 1},
-        {5, 4, 5}};
     // inputProcess(proc(esses, n);
-    // sort(processes[1][].begin(), processes[1].end());
-    CT(processes, n);
-    WT_TT(processes, n);
-    showProcess(processes, n);
+
+    FCFS(allProcesses, n);
 
     return 0;
 }
